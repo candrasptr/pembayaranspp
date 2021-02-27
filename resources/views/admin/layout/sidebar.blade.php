81164fb5333242fd1468190b6deac3c0fd34d620
candrasptr <csaputra2412@gmail.com>
2021-02-27 12:00:18 +0800

    first commit

---
+++ resources/views/admin/layout/sidebar.blade.php
@@ -0,0 +1,15 @@
+<div class="main-sidebar">
+  <aside id="sidebar-wrapper">
+    <div class="sidebar-brand">
+      <a href="index.html">Ichibanresto</a>
+    </div>
+    <div class="sidebar-brand sidebar-brand-sm">
+      <a href="index.html">Ichi</a>
+    </div>
+      <ul class="sidebar-menu">
+        {{-- dashboard --}}
+        <li class="menu-header">Dashboard</li>
+        <li class="@yield('dashboard')"><a class="nav-link" href="/dashboard"><i class="fas fa-fire"></i> <span>Dashboard</span></a></li>
+      </ul>
+  </aside>
+</div>
